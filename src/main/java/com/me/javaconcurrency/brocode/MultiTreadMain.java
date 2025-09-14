0fa45333e158e17eb12624ee56897f3539c5ea05
sudikonda <sudikonda.sivakumar@gmail.com>
2025-09-14 07:46:59 -0400

    feat: Multi Thread ping pong - Thread Join

--- src/main/java/com/me/javaconcurrency/brocode/MultiTreadMain.java
+++ src/main/java/com/me/javaconcurrency/brocode/MultiTreadMain.java
@@ -8,12 +8,23 @@ public class MultiTreadMain {
         // Thread = A set of instructions that runs independently
         // Useful for background tasks for time-consuming operations
 
-        MyRunnable myRunnable = new MyRunnable();
+        MyRunnable myRunnable = new MyRunnable("PING");
         Thread thread1 = new Thread(myRunnable);
-        Thread thread2 = new Thread(new MyRunnable());
+        Thread thread2 = new Thread(new MyRunnable("PONG"));
+
+        System.out.println("Game Starts !!");
 
         thread1.start();
         thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main Thread was interrupted");
+        }
+
+        System.out.println("Game Over !!");
 
     }
 }
--- src/main/java/com/me/javaconcurrency/brocode/MyRunnable.java
+++ src/main/java/com/me/javaconcurrency/brocode/MyRunnable.java
@@ -2,12 +2,18 @@ package com.me.javaconcurrency.brocode;
 
 public class MyRunnable implements Runnable {
 
+    private String text;
+
+    public MyRunnable(String text) {
+        this.text = text;
+    }
+
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
             try {
                 Thread.sleep(1000);
-                System.out.println(Thread.currentThread().getName() + " " + i);
+                System.out.println(Thread.currentThread().getName() + " " + text);
             } catch (InterruptedException e) {
                 System.out.println("Thread was interrupted");
             }
